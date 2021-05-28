c61f582b250d69f5bda99ec45d99e3b5a33a640d
Josh Fusillo <josh.fusillo@gmail.com>
2021-05-27 17:32:55 -0700

    made contact form required before submit

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,24 +1,29 @@
-import emailjs from "emailjs-com";
 
-  function sendEmail(e) {
-    e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_mxjx3iz",
-        "template_wiw8kem",
-        e.target,
-        "user_rWCesWhsBB4eCL8VQAOuJ"
-      )
-      .then(
-        (result) => {
-          alert("Thank you for the contact details - I will reach out shortly!");
+
+(function () {
+ 
+  emailjs.init("user_rWCesWhsBB4eCL8VQAOuJ");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      
+      // these IDs from the previous steps
+      emailjs.sendForm("service_mxjx3iz", "template_wiw8kem", this).then(
+        function () {
+          console.log("SUCCESS!");
+          alert(
+            "Thanks for your contact information - I will get back to you shortly!"
+          );
         },
-        (error) => {
-          console.log(error.text);
+        function (error) {
+          console.log("FAILED...", error);
         }
       );
-    e.target.reset();
-  }
-
-
+      event.target.reset();
+    });
+};
