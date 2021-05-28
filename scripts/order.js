0aa624cb0e14c5a91e7862bf071edc105a858e85
Josh Fusillo <josh.fusillo@gmail.com>
2021-05-27 17:11:46 -0700

    order form button disabled until address and order item 1 is picked

--- scripts/order.js
+++ scripts/order.js
@@ -1,44 +1,51 @@
 
 
-let form = document.querySelector('form')
-let formObject = form.addEventListener('submit', e => {
-    e.preventDefault();
+let checkform = () => {  
+  let f = document.forms["order-form"].elements;
+  let cansubmit = true;
+  for (let i = 0; i < f.length; i++) {
+      if (f[i].value.length == 0)(f[i].value == null)
+          cansubmit = false;      
 
-    
+  }
 
-    let firstName=document.querySelector('#firstName').value;
-    let lastName=document.querySelector('#lastName').value;
-    let emailAddress=document.querySelector('#emailAddress').value;
-    let phoneNumber=document.querySelector('#phoneNumber').value;
-    let addressOne=document.querySelector('#addressOne').value;
-    let addressTwo=document.querySelector('#addressTwo').value;
-    let city=document.querySelector('#city').value;
-    let province=document.querySelector('#province').value;
-    let postalCode=document.querySelector('#postalCode').value;
-    let itemOne=document.querySelector('#itemOne').value;
-    let itemTwo=document.querySelector('#itemTwo').value;
-    let itemThree=document.querySelector('#itemThree').value;
-    let itemFour=document.querySelector('#itemFour').value; 
-     
-
-    const orderInfo = { 
-        firstName,
-        lastName,
-        emailAddress,
-        phoneNumber,
-        addressOne,
-        addressTwo,
-        city,
-        province,
-        postalCode,
-        itemOne,
-        itemTwo,
-        itemThree,
-        itemFour
-    }
-
-    console.log(orderInfo)
-
+  document.querySelector('#submitbutton').disabled = !cansubmit;
 }
-)
-    
+window.onload = checkform;
+
+function enableButton()
+{
+    var selectelem = document.getElementById('itemOne');
+    var btnelem = document.getElementById('submitbutton');
+    btnelem.disabled = !selectelem.value;
+}
+
+//Email user info
+(function () {
+  emailjs.init("user_rWCesWhsBB4eCL8VQAOuJ");
+})();
+
+
+//Send email to emailJS
+window.onload = function () {
+  document
+    .querySelector(".order-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();     
+      emailjs.sendForm("service_mxjx3iz", "template_x7tr2pe", this).then(
+        function () {
+          console.log("SUCCESS!");
+          alert(
+            "We have received your order!  You will hear back within 24-48 hours!"
+          );
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+        
+      );
+      event.target.reset();
+    });
+};
+
+
