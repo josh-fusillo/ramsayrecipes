5c97f092939354ee45066f5f367e9baf1ba7c4e0
shadow104 <josh.fusillo@gmail.com>
2021-09-01 13:42:51 -0700

    updated contact form email code

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,39 +4,6 @@ let contactSend = () => {
     setTimeout(() => $("#contactModal").modal("hide"), 2500)
 }
 
-// api to emailJS
 
-window.onload = () => {
-  document
-  .querySelector('.contactform')
-  .addEventListener("submit", sendEmail = (event) => {
-  event.preventDefault();
-  
-    let data = {
-      service_id: 'service_mxjx3iz',
-      template_id: 'template_wiw8kem',
-      user_id: 'user_rWCesWhsBB4eCL8VQAOuJ',
-      template_params: {          
-          'name': event.target.name.value,
-          'email': event.target.email.value,
-          'phone': event.target.phone.value,
-          'message': event.target.message.value        
-          
-      }
-    };
-    
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-      type: 'POST',
-      data: JSON.stringify(data),
-      contentType: 'application/json'
-    }).done(function() {
-      contactSend();
-      event.target.reset();
-      console.log('Your mail is sent!');
-    }).fail(function(error) {
-      console.log('Oops... ' + JSON.stringify(error));
-    });
-  })
-}
 
 
